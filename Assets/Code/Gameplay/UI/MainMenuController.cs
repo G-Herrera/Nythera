30365fb39af11d544fd8712de999ea2c55f5d24b
Gerardo Herrera <gerardohhq@outlook.com>
2026-07-16 10:57:24 -0600

    Add menu input navigation and build scenes
    MainMenuController now uses the Input System (PlayerInputActions) to handle UI Navigate and Submit, moving the menu selector and invoking Start/Options/Exit actions (Start loads 'scn_PlayerController'). Selector position is adjusted visually and helper methods for MoveUp/MoveDown are added. Project build settings updated to include scn_MainMenu and scn_PlayerController so those scenes are included in the build.

--- Assets/Code/Gameplay/UI/MainMenuController.cs
+++ Assets/Code/Gameplay/UI/MainMenuController.cs
@@ -1,6 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.ShaderGraph;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
 
 public class MainMenuController : MonoBehaviour
 {
@@ -9,6 +14,27 @@ public class MainMenuController : MonoBehaviour
     [SerializeField] private RectTransform[] menuOptions;
 
     private int currentOptionIndex = 0;
+    private PlayerInputActions inputActions;
+    private Vector2 navigateInput;
+
+    private void Awake()
+    {
+        inputActions = new PlayerInputActions();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.UI.Enable();
+        inputActions.UI.Navigate.started += OnNavigate;
+        inputActions.UI.Submit.performed += OnSubmit;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.UI.Disable();
+        inputActions.UI.Navigate.started -= OnNavigate;
+        inputActions.UI.Submit.performed -= OnSubmit;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +53,86 @@ public class MainMenuController : MonoBehaviour
         if ( selector != null && menuOptions.Length > 0)
         {
             selector.position = menuOptions[currentOptionIndex].position;
+            Vector3 pos = menuOptions[currentOptionIndex].position;
+            pos.x -= 290f;   // o el valor que visualmente se vea bien
+            pos.y += 15f;    // o el valor que visualmente se vea bien
+            selector.position = pos;
         }
+    }
+
+    private void OnNavigate(InputAction.CallbackContext context)
+    {
+        navigateInput = context.ReadValue<Vector2>();
+
+        if (navigateInput.y > 0f)
+        {
+            // Move up in the menu
+            MoveUp();
+        } 
+        else if (navigateInput.y < 0f)
+        {
+            // Move down in the menu
+            MoveDown();
+        }
+    }
+
+    private void OnSubmit(InputAction.CallbackContext context)
+    {
+        // Handle menu option selection based on currentOptionIndex
+        switch (currentOptionIndex)
+        {
+            case 0:
+                // Start Game
+                StartNewGame();
+                break;
+            case 1:
+                // Options
+                OpenOptions();
+                break;
+            case 2:
+                // Exit
+                ExitGame();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void StartNewGame()
+    {
+        //Load the first level or main game scene
+        SceneManager.LoadScene("scn_PlayerController");
+    }
+
+    private void OpenOptions()
+    {
+        //Load the options panel or scene
+        //SceneManager.LoadScene("OptionsScene"); // Replace with your options scene name
+        Debug.Log("Options selected. Implement options functionality here.");
+    }
+
+    private void ExitGame()
+    {
+        //Exit the game
+        //Application.Quit();
+        Debug.Log("Exiting game. Implement exit functionality here.");
+    }
+
+
+
+    private void MoveUp()
+    {
+        if (currentOptionIndex == 0) return;
+
+            currentOptionIndex--;
+            UpdateSelector();   
+    }
+
+    private void MoveDown()
+    {
+        if (currentOptionIndex == menuOptions.Length - 1) return;
+
+        currentOptionIndex++;
+        UpdateSelector();
     }
 }
